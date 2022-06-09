afdd78171fefc4df527c7fa0b2ba1d968a5df66a
ChristianPastenC <christian_pasten@outlook.com>
2022-06-09 13:46:09 -0500

    change test cases display names

--- JUnitReview/src/test/java/com/junitreview/junitdemo/DemoUtilsTest.java
+++ JUnitReview/src/test/java/com/junitreview/junitdemo/DemoUtilsTest.java
@@ -1,14 +1,38 @@
 package com.junitreview.junitdemo;
 
 import org.junit.jupiter.api.*;
-
 import static org.junit.jupiter.api.Assertions.*;
 
+// @DisplayNameGeneration(DisplayNameGenerator.IndicativeSentences.class)
 class DemoUtilsTest {
 
     DemoUtils demoUtils;
 
-    @BeforeAll
+    @BeforeEach
+    void setupBeforeEach() {
+        demoUtils = new DemoUtils();
+    }
+
+    @Test
+    @DisplayName("Equals & not equals")
+    void testEqualsAndNotEquals() {
+        // System.out.println("Test Equals and not equals");
+        assertEquals(6, demoUtils.add(2,4), "2+4 must be 6");
+        assertNotEquals(6, demoUtils.add(1, 9), "1+9 must not be 6");
+    }
+
+    @Test
+    @DisplayName("Null & not null")
+    void testNullAndNotNull() {
+        // System.out.println("Test Null and not null");
+        String str1 = null;
+        String str2 = "junit review";
+
+        assertNull(demoUtils.checkNull(str1), "object should be null");
+        assertNotNull(demoUtils.checkNull(str2), "object should not be null");
+    }
+}
+/* @BeforeAll
     static void setupBeforeEachClass() {
         System.out.println("Executes before all test methods");
     }
@@ -18,31 +42,8 @@ class DemoUtilsTest {
         System.out.println("Executes after all test methods");
     }
 
-    @BeforeEach
-    void setupBeforeEach() {
-        demoUtils = new DemoUtils();
-        System.out.println("Executes before each test case");
-    }
-
     @AfterEach
     void tearDownAfterEach() {
         System.out.println("Executes after each test case");
     }
-
-    @Test
-    void testEqualsAndNotEquals() {
-        System.out.println("Test Equals and not equals");
-        assertEquals(6, demoUtils.add(2,4), "2+4 must be 6");
-        assertNotEquals(6, demoUtils.add(1, 9), "1+9 must not be 6");
-    }
-
-    @Test
-    void testNullAndNotNull() {
-        System.out.println("Test Null and not null");
-        String str1 = null;
-        String str2 = "junit review";
-
-        assertNull(demoUtils.checkNull(str1), "object should be null");
-        assertNotNull(demoUtils.checkNull(str2), "object should not be null");
-    }
-}
+*/
