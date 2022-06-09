0cca5431ec7e8ecd8d3c105f693066c0dc159bf7
ChristianPastenC <christian_pasten@outlook.com>
2022-06-09 13:20:15 -0500

    add before & after functions

--- JUnitReview/src/test/java/com/junitreview/junitdemo/DemoUtilsTest.java
+++ JUnitReview/src/test/java/com/junitreview/junitdemo/DemoUtilsTest.java
@@ -1,27 +1,48 @@
 package com.junitreview.junitdemo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-public class DemoUtilsTest {
+class DemoUtilsTest {
+
+    DemoUtils demoUtils;
+
+    @BeforeAll
+    static void setupBeforeEachClass() {
+        System.out.println("Executes before all test methods");
+    }
+
+    @AfterAll
+    static void tearDownAfterAll() {
+        System.out.println("Executes after all test methods");
+    }
+
+    @BeforeEach
+    void setupBeforeEach() {
+        demoUtils = new DemoUtils();
+        System.out.println("Executes before each test case");
+    }
+
+    @AfterEach
+    void tearDownAfterEach() {
+        System.out.println("Executes after each test case");
+    }
+
     @Test
     void testEqualsAndNotEquals() {
-        // set up
-        DemoUtils demoUtils = new DemoUtils();
-
-        // assert
+        System.out.println("Test Equals and not equals");
         assertEquals(6, demoUtils.add(2,4), "2+4 must be 6");
-        assertNotEquals(8, demoUtils.add(2,4), "2+4 must not be 8");
+        assertNotEquals(6, demoUtils.add(1, 9), "1+9 must not be 6");
     }
 
     @Test
     void testNullAndNotNull() {
-        DemoUtils demoUtils = new DemoUtils();
-
+        System.out.println("Test Null and not null");
         String str1 = null;
         String str2 = "junit review";
 
-        assertNull(demoUtils.checkNull(str1), "Object should be null");
-        assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
+        assertNull(demoUtils.checkNull(str1), "object should be null");
+        assertNotNull(demoUtils.checkNull(str2), "object should not be null");
     }
 }
