ca843c2df9851e970c816d219ce1f16222a81501
ChristianPastenC <christian_pasten@outlook.com>
2022-06-09 12:15:43 -0500

    check how to assert works

--- JUnitReview/src/main/java/com/junitreview/junitdemo/DemoUtils.java
+++ JUnitReview/src/main/java/com/junitreview/junitdemo/DemoUtils.java
@@ -4,4 +4,10 @@ public class DemoUtils {
     public int add(int a, int b) {
         return (a+b);
     }
+    public Object checkNull(Object obj){
+        if (obj != null) {
+            return obj;
+        }
+        return null;
+    }
 }
--- JUnitReview/src/test/java/com/junitreview/junitdemo/DemoUtilsTest.java
+++ JUnitReview/src/test/java/com/junitreview/junitdemo/DemoUtilsTest.java
@@ -1,7 +1,7 @@
 package com.junitreview.junitdemo;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class DemoUtilsTest {
     @Test
@@ -9,12 +9,19 @@ public class DemoUtilsTest {
         // set up
         DemoUtils demoUtils = new DemoUtils();
 
-        int expected = 6;
-
-        // execute
-        int actual = demoUtils.add(2, 4);
-
         // assert
-        Assertions.assertEquals(expected, actual, "2+4 must be 6");
+        assertEquals(6, demoUtils.add(2,4), "2+4 must be 6");
+        assertNotEquals(8, demoUtils.add(2,4), "2+4 must not be 8");
+    }
+
+    @Test
+    void testNullAndNotNull() {
+        DemoUtils demoUtils = new DemoUtils();
+
+        String str1 = null;
+        String str2 = "junit review";
+
+        assertNull(demoUtils.checkNull(str1), "Object should be null");
+        assertNotNull(demoUtils.checkNull(str2), "Object should not be null");
     }
 }
