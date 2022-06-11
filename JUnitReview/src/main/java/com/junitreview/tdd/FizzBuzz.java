7d57874c6cf8066c6998bdd3c8a51473464621fb
ChristianPastenC <christian_pasten@outlook.com>
2022-06-11 10:43:00 -0500

    Refactor FizzBuzz Class

--- JUnitReview/src/main/java/com/junitreview/tdd/FizzBuzz.java
+++ JUnitReview/src/main/java/com/junitreview/tdd/FizzBuzz.java
@@ -5,6 +5,25 @@ public class FizzBuzz {
     // If number is divisible by 5, print Buzz
     // If number is divisible by 3 and 5 (15), print FizBuzz
     // If number is NOT divisible by 3 or 5, then print the number
+
+    public static String compute(int i) {
+        StringBuilder result = new StringBuilder();
+        if ( i % 3 == 0){
+            result.append("Fizz");
+        }
+
+        if ( i % 5 == 0){
+            result.append("Buzz");
+        }
+
+        if ( result.length() == 0 ){
+            result.append(i);
+        }
+
+        return result.toString();
+    }
+
+    /*
     public static String compute(int i) {
         if( (i % 3 == 0 ) && (i % 5 == 0)){
             return "FizzBuzz";
@@ -17,4 +36,6 @@ public class FizzBuzz {
             return Integer.toString(i);
         }
     }
+    */
+
 }
