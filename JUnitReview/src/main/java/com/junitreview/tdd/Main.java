6cb1738da2c0f5169773c98179976d9269ed35ca
ChristianPastenC <christian_pasten@outlook.com>
2022-06-11 11:48:23 -0500

    add Main Class

---
+++ JUnitReview/src/main/java/com/junitreview/tdd/Main.java
@@ -0,0 +1,9 @@
+package com.junitreview.tdd;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(FizzBuzz.compute(i));
+        }
+    }
+}
