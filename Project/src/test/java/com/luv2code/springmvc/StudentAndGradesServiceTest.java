21507d99c492e31becde337c9f4f81753b98b6d4
ChristianPastenC <christian_pasten@outlook.com>
2022-06-21 16:54:59 -0500

    Create service and repository

--- Project/src/test/java/com/luv2code/springmvc/StudentAndGradesServiceTest.java
+++ Project/src/test/java/com/luv2code/springmvc/StudentAndGradesServiceTest.java
@@ -1,20 +1,40 @@
 package com.luv2code.springmvc;
 
 import com.luv2code.springmvc.models.CollegeStudent;
+import com.luv2code.springmvc.repository.StudentDao;
+import com.luv2code.springmvc.service.StudentAndGradeService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @TestPropertySource("/application.properties")
 @SpringBootTest
 public class StudentAndGradesServiceTest {
+    @Autowired
+    private StudentAndGradeService studentService;
+
+     @Autowired
+     private StudentDao studentDao;
 
     @Test
     public void createStudentService(){
-        //studentService.createStudent("Christian", "Pasten", "christian_pasten@outlook.com");
+        studentService.createStudent(
+                "Christian",
+                "Pasten",
+                "christian_pasten@outlook.com"
+        );
 
-        // CollegeStudent student = studentDao.findByEmailAddress("christian_pasten@outlook.com");
+         CollegeStudent student = studentDao.findByEmailAddress(
+                 "christian_pasten@outlook.com"
+         );
 
-        // assertEquals("christian_pasten@outlook.com", student.getEmailAddress(), "find by email");
+         assertEquals(
+                 "christian_pasten@outlook.com",
+                 student.getEmailAddress(),
+                 "find by email"
+         );
     }
 }
